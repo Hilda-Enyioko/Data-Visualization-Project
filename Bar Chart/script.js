394ce1d87158b6fa3d25f23e8bd04fb79896dc28
Hilda Enyioko <enyiokohilda@gmail.com>
2025-03-01 19:20:14 +0100

    api test

--- Bar Chart/script.js
+++ Bar Chart/script.js
@@ -1,1 +1,7 @@
 import * as d3 from "https://cdn.jsdelivr.net/npm/d3@7/+esm";
+
+const barChart = document.getElementById('bar-chart');
+
+fetch('https://raw.githubusercontent.com/freeCodeCamp/ProjectReferenceData/master/GDP-data.json')
+.then(response => response.json())
+.then(data => console.log(data));
