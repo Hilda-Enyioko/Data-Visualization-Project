8f625b5cf741a339af5c2d0e2ad68479ef40d963
Hilda Enyioko <enyiokohilda@gmail.com>
2025-03-02 11:52:05 +0100

    test values update

--- Bar Chart/script.js
+++ Bar Chart/script.js
@@ -1,7 +1,10 @@
 import * as d3 from "https://cdn.jsdelivr.net/npm/d3@7/+esm";
 
 const barChart = document.getElementById('bar-chart');
+const values = [];
 
 fetch('https://raw.githubusercontent.com/freeCodeCamp/ProjectReferenceData/master/GDP-data.json')
 .then(response => response.json())
-.then(data => console.log(data));
+.then(data => {
+    values = values.concat(data[0].data);
+});
