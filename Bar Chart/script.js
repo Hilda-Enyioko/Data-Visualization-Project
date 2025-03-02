7f9e9be804f5dddb15709b53b68ff671db6b9531
Hilda Enyioko <enyiokohilda@gmail.com>
2025-03-02 12:43:07 +0100

    fetch api using XMLHttpRequest

--- Bar Chart/script.js
+++ Bar Chart/script.js
@@ -3,9 +3,10 @@ import * as d3 from "https://cdn.jsdelivr.net/npm/d3@7/+esm";
 const barChart = document.getElementById('bar-chart');
 let values = [];
 
-fetch('https://raw.githubusercontent.com/freeCodeCamp/ProjectReferenceData/master/GDP-data.json')
-.then(response => response.json())
-.then(data => {
-    values = values.concat(data[0].data);
-    console.log(values);
-});
+const req = new XMLHttpRequest();
+req.open("GET", 'https://raw.githubusercontent.com/freeCodeCamp/ProjectReferenceData/master/GDP-data.json', true);
+req.send();
+req.onload = () => {
+    const json = JSON.parse(req.responseText);
+    console.log(json);
+};
