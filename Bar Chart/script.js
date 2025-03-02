3ecffe1aaa7c742691eeac6df72d215e5db6c3e5
Hilda Enyioko <enyiokohilda@gmail.com>
2025-03-02 11:57:26 +0100

    correct variable 'values' declaration

--- Bar Chart/script.js
+++ Bar Chart/script.js
@@ -1,7 +1,7 @@
 import * as d3 from "https://cdn.jsdelivr.net/npm/d3@7/+esm";
 
 const barChart = document.getElementById('bar-chart');
-const values = [];
+let values = [];
 
 fetch('https://raw.githubusercontent.com/freeCodeCamp/ProjectReferenceData/master/GDP-data.json')
 .then(response => response.json())
