9af7606277b560c6fbcf4d5b9d86f9ce9b847488
Hilda Enyioko <enyiokohilda@gmail.com>
2025-03-23 00:06:43 +0100

    test map so far

--- Heat Map/script.js
+++ Heat Map/script.js
@@ -1,6 +1,9 @@
+import * as d3 from "https://cdn.jsdelivr.net/npm/d3@7/dist/d3.min.js";
+
 const heatMap = document.getElementById('heat-map');
 
 let dataset = [];
+let baseTemp = "";
 
 const req = new XMLHttpRequest();
 
@@ -8,8 +11,10 @@ req.open("GET", 'https://raw.githubusercontent.com/freeCodeCamp/ProjectReference
 req.send();
 
 req.onload = () => {
-    dataset = JSON.parse(req.responseText);
-    console.log('DATASET:\n', dataset);
+    const json = JSON.parse(req.responseText);
+    dataset = json.monthlyVariance;
+    baseTemp = parseFloat(json.baseTemperature);
+    console.log('DATASET:\n', dataset, "\nBase Temperature:\n", baseTemp);
 
     plotMap();
 }
@@ -17,17 +22,51 @@ req.onload = () => {
 function plotMap() {
 
     const padding = 40;
-    const width = (1.5 * heatMap.clientWidth);
+    const width = (1.8 * heatMap.clientWidth);
     const height = heatMap.clientWidth;
 
-    const svgMap = heatMap.append('svg')
-                          .attr('width', width)
-                          .attr('height', height)
-                          .attr('id', 'svg-map');
+    const minYear = d3.min(dataset, d => d.year);
+    const maxYear = d3.max(dataset, d => d.year);
+
+    const minMonth = d3.min(dataset, d => d.month);
+    const maxMonth = d3.max(dataset, d => d.month);
+
+    const cellWidth = (width - (2 * padding)) / (maxYear - minYear + 1);
+    const cellHeight = (height - padding) / (maxMonth - minMonth + 1);
+
+    //x-dcale for years
+    const xScale = d3.scaleLinear()
+                     .domain([minYear, maxYear])
+                     .range([padding, width - padding]);
+    
+    // y-scale for months
+    const yScale = d3.scaleBand()
+                     .domain(d3.range(12))
+                     .range([padding, height - padding]);
+
+    // color scale for temperature
+    const colorScale = d3.scaleSequential(d3.interpolateRdYlBu.reverse())
+                         .domain(d3.extent(dataset, d => baseTemp + d.variance));
+
+    const svgMap = d3.select(heatMap)
+                     .append('svg')
+                     .attr('width', width)
+                     .attr('height', height)
+                     .attr('id', 'svg-map');
     
     svgMap.selectAll('rect')
           .data(dataset)
           .enter()
-          .append('rect');
+          .append('rect')
+          .attr('class', 'cell')
+          .attr('width', cellWidth)
+          .attr('height', cellHeight)
+          .attr('x', d => xScale(d.year))
+          .attr('y', d => yScale(d.month - 1))
+          .attr('data-month', d => d.month)
+          .attr('data-year', d => d.year)
+          .attr('data-temp', d => (d.variance + baseTemp).toFixed(2))
+          .attr('fill', d => colorScale(baseTemp + d.variance));
+    
 
 }
