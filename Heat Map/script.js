7d6f8d04078fc05b7c55b5b9f287fe5f16e8cd19
Hilda Enyioko <enyiokohilda@gmail.com>
2025-03-26 17:38:57 +0100

    correct use of reference function  d3.interpolateRdYlBu

--- Heat Map/script.js
+++ Heat Map/script.js
@@ -43,8 +43,11 @@ function plotMap() {
                      .range([padding, height - padding]);
 
     // color scale for temperature
-    const colorScale = d3.scaleSequential(d3.interpolateRdYlBu())
-                         .domain([d3.max(dataset, d => d.variance + baseTemp), d3.min(dataset, d => d.variance + baseTemp)]);
+    const colorScale = d3.scaleSequential(d3.interpolateRdYlBu)
+                         .domain([
+                            d3.max(dataset, d => d.variance + baseTemp), 
+                            d3.min(dataset, d => d.variance + baseTemp)
+                        ]);
 
     const svgMap = d3.select(heatMap)
                      .append('svg')
