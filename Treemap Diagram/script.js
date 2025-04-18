2a3a50a1008420d6dfb3ce023f2fe820449bdc88
Hilda Enyioko <enyiokohilda@gmail.com>
2025-04-18 09:04:37 +0100

    maintain heading version

--- Treemap Diagram/script.js
+++ Treemap Diagram/script.js
@@ -2,32 +2,31 @@ const dropdown = document.getElementById("dropdown");
 const treemap = document.getElementById("treemap");
 let dropdownValue = "";
 
-const heading = [
-    {
+const heading = {
+    "default": {
         title: "Kickstarter Pledges",
         description: "Top 100 Most Pledged Kickstarter Campaigns Grouped By Category"
     },
-    {
+
+    "video": {
         title: "Video Game Sales",
         description: "Top 100 Most Sold Video Games Grouped by Platform"
     },
-    {
+    
+    "movie": {
         title: "Movie Sales",
         description: "Top 100 Highest Grossing Movies Grouped By Genre"
     }
-];
-
-const headingMap = {
-    "default": 0,
-    "video-game": 1,
-    "movie": 2
-}
+};
 
 dropdown.addEventListener("change", function() {
     dropdownValue = dropdown.value;
     console.log(dropdownValue);
-    const selectedHeading = heading[headingMap[dropdownValue]];
+
+    const selectedHeading = heading[dropdownValue];
+
     document.getElementById("heading").textContent = selectedHeading.title;
     document.getElementById("description").textContent = selectedHeading.description;
+    
     treemap.innerHTML = "";
 });
